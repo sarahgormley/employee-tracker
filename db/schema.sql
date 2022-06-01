62a67be05b3fc7d9c8636b849d392b32ad0a77d0
SarahGormley <sarah.gormley39@gmail.com>
2022-06-01 17:11:06 +1000

    Schema and seeds done

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    dept_name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL, 
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id),
+    ON DELETE SET NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (dept_name)
+VALUES ("Sales"),
+("Engineering"),
+("Finance"),
+("Legal");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1),
+("Salesperson", 80000, 1),
+("Lead Engineer", 150000, 2),
+("Software Engineer", 120000, 2),
+("Account Manager", 160000, 3),
+("Accountant", 125000, 3),
+("Legal Team Lead", 250000, 4),
+("Lawyer", 190000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 1)
+("Mike", "Chan", 2, 1)
+("Ashley", "Smith", 3)
+("Kevin", "Miles", 4, 3)
+("Kunal", "Singh", 5)
+("Malia", "Brown", 6, 5)
+("Sarah", "Anne", 7)
+("Damien", "Jinks", 8, 7);
